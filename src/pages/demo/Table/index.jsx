94821af581f6d210d6d44b0f458cd4a2c0fb88f4
jinwei233 <jinwei233@github.com>
2017-12-06 10:34:44 +0800

    latex demo click

--- src/pages/demo/Table/index.jsx
+++ src/pages/demo/Table/index.jsx
@@ -1,20 +1,26 @@
 import React, { Component } from 'react';
+import { imgTexSrc } from '../util';
 
 import './index.scss';
 
 class Table extends Component {
   render() {
+    const items = this.props.items || ['a^2+b^2'];
     return (
       <div className="demo-table">
         <h2>化学</h2>
         <table>
           <tbody>
             <tr>
-              <td>a^2+b^2</td>
-              <td>a^2+b^2</td>
-              <td>a^2+b^2</td>
-              <td>a^2+b^2</td>
-              <td>a^2+b^2</td>
+              {
+                items.map((item) => {
+                  return (
+                    <td key={item}>
+                      <img onClick={this.props.onClickItem} alt={item} src={imgTexSrc(item)} />
+                    </td>
+                  );
+                })
+              }
             </tr>
           </tbody>
         </table>
--- src/pages/demo/index.js
+++ src/pages/demo/index.js
@@ -10,12 +10,17 @@ class Demo extends Component {
   componentDidMount() {
     console.log('mounted');
   }
+  onClickItem = (e) => {
+    const img = e.currentTarget;
+    const latex = img.getAttribute('alt');
+    console.log(latex);
+  }
   render() {
     return (
       <div className="editor-cont">
         <h1 className="editor-demo-title">带公式编辑能力的富文本编辑器</h1>
         <Editor />
-        <Table />
+        <Table onClickItem={this.onClickItem} />
       </div>
     );
   }
