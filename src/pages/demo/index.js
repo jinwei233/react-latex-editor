0496ec7651ce8028843bb6b893fc4c1d079366a2
jinwei233 <jinwei233@github.com>
2017-12-06 11:57:59 +0800

    数学的 demo ok

--- src/pages/demo/index.js
+++ src/pages/demo/index.js
@@ -9,6 +9,120 @@ import './index.scss';
 
 
 const demos = [{
+  title: '数学',
+  key: uuid.next(),
+  rows: [{
+    key: uuid.next(),
+    items: [
+      'a^2 + b^2 = c^2',
+      '\\lim_{n \\to \\infty}\\sum_{k=1}^n \\frac{1}{k^2}= \\frac{\\pi^2}{6}',
+      '\\forall x \\in \\mathbf{R}:x^{2} \\geq 0',
+      'a^x+y \\neq a^{x+y}',
+    ],
+  }, {
+    key: uuid.next(),
+    items: [
+      'n! = 1 \\cdot 2 \\cdots (n-1) \\cdot n',
+      '0.\\overline{3} = \\underline{\\underline{1/3}}',
+      '\\underbrace{\\overbrace{a+b+c}^6 \\cdot \\overbrace{d+e+f}^7} _\\text{meaning of life} = 42',
+      '-',
+    ],
+  }, {
+    key: uuid.next(),
+    items: [
+      'f(x) = x^2',
+      'f’(x) = 2x',
+      'f’’(x) = 2',
+      '-',
+    ],
+  }],
+}, {
+  key: uuid.next(),
+  rows: [{
+    key: uuid.next(),
+    items: [
+      '\\hat{XY}',
+      '\\widehat{XY}',
+      '\\bar{x_0}',
+      '\\bar{x}_0',
+      '\\vec{AB}',
+      '\\overrightarrow{AB}',
+      'x\\equiv a \\pmod{b}',
+    ],
+  }],
+}, {
+  key: uuid.next(),
+  rows: [{
+    key: uuid.next(),
+    items: [
+      '3/8 \\quad \\frac{3}{8} \\quad \\tfrac{3}{8}',
+      '\\binom{n}{k} =\\binom{n-1}{k} + \\binom{n-1}{k-1}',
+      '\\sum^n_{\\substack{0<i<n \\\\ j\\subseteq i}} P(i,j) = Q(i,j)',
+    ],
+  }],
+}, {
+  key: uuid.next(),
+  rows: [{
+    key: uuid.next(),
+    items: [
+      `
+\\begin{align}
+a & = b + c \\\\
+& = d + e
+\\end{align}
+`,
+      `
+a + b + c + d + e + f
++ g + h + i
+\\\\
+= j + k + l + m + n
+`,
+    ],
+  }],
+}, {
+  key: uuid.next(),
+  rows: [{
+    key: uuid.next(),
+    items: [
+      `
+\\mathbf{X} = \\left(
+\\begin{array}{ccc}
+x_1 & x_2 & \\ldots \\\\
+x_3 & x_4 & \\ldots \\\\
+\\vdots & \\vdots & \\ddots
+\\end{array} \\right)
+`,
+      `
+|x| =
+\\begin{cases}
+-x & \\text{if } x < 0,\\\\
+0 & \\text{if } x = 0,\\\\
+x & \\text{if } x > 0.
+\\end{cases}
+`,
+    ],
+  }],
+}, {
+  key: uuid.next(),
+  rows: [{
+    key: uuid.next(),
+    items: [
+      `\\begin{bmatrix}
+p_{11} & p_{12} & \\ldots
+& p_{1n} \\\\
+p_{21} & p_{22} & \\ldots
+& p_{2n} \\\\
+\\vdots & \\vdots & \\ddots
+& \\vdots \\\\
+p_{m1} & p_{m2} & \\ldots
+& p_{mn}
+\\end{bmatrix}
+`,
+      '\\int_1^2 \\ln x \\,\\mathrm{d}x',
+      '\\iint f(x)g(y) \\mathrm{d} x \\mathrm{d} y',
+    ],
+  }],
+}, {
   title: '化学',
   key: uuid.next(),
   rows: [{
